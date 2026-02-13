166079e398765b521f9f64e07688b862fd6b41c3
Usman / <usmanrehmani126@gmail.com>
2026-02-14 00:13:30 +0500

    added prisma client fixes warning

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    super({
+      adapter,
+      log:
+        process.env.NODE_ENV === 'development'
+          ? ['query', 'error', 'warn']
+          : ['error'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Database connected successfully!');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('Database disconnected!');
+  }
+
+  async cleanDatabase() {
+    if (process.env.NODE_ENV === 'production') {
+      throw new Error('Cannot clean database in production');
+    }
+
+    const models = Reflect.ownKeys(this).filter(
+      (key) => typeof key === 'string' && !key.startsWith('_'),
+    );
+
+    return Promise.all(
+      models.map((modelKey) => {
+        if (typeof modelKey === 'string') {
+          return this[modelKey].deleteMany();
+        }
+      }),
+    );
+  }
+}
