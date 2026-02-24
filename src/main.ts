9ee8042c220d564fb50edae61b0fa588478c5ef6
Usman / <usmanrehmani126@gmail.com>
2026-02-24 22:51:59 +0500

    fixes database URL issue in env update the prisma commands to sync the tables with db and now start working on CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('/api/v1/');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
